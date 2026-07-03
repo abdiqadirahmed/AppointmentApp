5447a4e37f4dad4399ef208413c36fed81d40564
Abdiqadirdesigns <132311786+AbdiqadirDesigns@users.noreply.github.com>
2026-07-03 23:21:04 +0300

    Modified the Settings 3rd time

--- backend/appointmentapp/settings.py
+++ backend/appointmentapp/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production-xyz123')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ['44.204.111.130/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['44.204.111.130/8000']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
